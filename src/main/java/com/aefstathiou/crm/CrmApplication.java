1ee675bb91e2c4f767bad9e6753ecb932d02b571
Athanasios Efstathiou <a.efstathiou@proton.me>
2025-07-12 11:24:43 +0300

    Initial commit

---
+++ src/main/java/com/aefstathiou/crm/CrmApplication.java
@@ -0,0 +1,15 @@
+package com.aefstathiou.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("secrets.properties")
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+
+}
