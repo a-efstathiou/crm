632df539b68b2e3b892a6300f87083a376a4de56
Athanasios Efstathiou <a.efstathiou@proton.me>
2025-07-19 21:48:01 +0300

    added bootstrap + apis with token

---
+++ frontend/src/services/tokenService.js
@@ -0,0 +1,34 @@
+class TokenService {
+    constructor() {
+        this.accessToken = null;
+    }
+
+    getLocalRefreshToken() {
+        const user = JSON.parse(localStorage.getItem("user"));
+        return user?.refresh_token;
+    }
+
+    getLocalAccessToken() {
+        return this.accessToken;
+    }
+
+    updateLocalAccessToken(token) {
+        this.accessToken = token;
+    }
+
+    getUser() {
+        return JSON.parse(localStorage.getItem("user"));
+    }
+
+    setUser(user) {
+        const { access_token, ...userData } = user;
+        localStorage.setItem("user", JSON.stringify(userData));
+        this.updateLocalAccessToken(access_token);
+    }
+
+    removeUser() {
+        localStorage.removeItem("user");
+    }
+}
+
+export default new TokenService();
