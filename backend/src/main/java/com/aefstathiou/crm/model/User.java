6b98b84387d73f43ed88b73acb893f98400170b2
Athanasios Efstathiou <a.efstathiou@proton.me>
2025-07-19 22:24:15 +0300

    changed table name of role to roles

--- backend/src/main/java/com/aefstathiou/crm/model/User.java
+++ backend/src/main/java/com/aefstathiou/crm/model/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     private String email;
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "role")
+    @Column(name = "roles")
     private List<Role> roles;
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
