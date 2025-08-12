84aa94d7acd23aaed17bd8382a10fbe99af76c26
Athanasios Efstathiou <a.efstathiou@proton.me>
2025-08-12 12:17:05 +0300

    removed comment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -12,7 +12,7 @@ instance.interceptors.request.use(
     (config) => {
         const token = TokenService.getLocalAccessToken();
         if (token) {
-            config.headers["Authorization"] = 'Bearer ' + token;  // for Spring Boot back-end
+            config.headers["Authorization"] = 'Bearer ' + token;
         }
         return config;
     },
